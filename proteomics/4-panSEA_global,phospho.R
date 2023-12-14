5e2d195547ddf75c6d6da22c9ab32887e4232ea8
Belinda B. Garana <75867404+BelindaBGarana@users.noreply.github.com>
2023-12-14 11:56:32 -0800

    Add synapse IDs to get BeatAML data from Synapse directly

--- proteomics/4-panSEA_global,phospho.R
+++ proteomics/4-panSEA_global,phospho.R
@@ -1,52 +1,53 @@
 # Differential expression & enrichment analyses: global & phospho
-# PTRC2: exp 21, 12, 18
+# PTRC2: exp 21
 # Author: Belinda B. Garana
 # Created: 2023-12-06
-# Last edit: 2023-12-06
+# Last edit: 2023-12-14
 
-library(readxl);library(panSEA)
+library(readxl);library(panSEA); library(synapser)
 
 #### 1. Import metadata & crosstabs ####
-setwd("~/OneDrive - PNNL/Documents/GitHub/Exp21_NRAS_ASO_treated_patients/proteomics/data/")
-
-meta.df <- readxl::read_excel("Experiment 21 - Patient Samples_F.xlsx", sheet = 2)
-global.df <- read.table("global_data/ptrc_ex21_crosstab_global_gene_corrected.txt", sep = "\t")
-#phospho.df <- read.table("phospho_data/ptrc_ex21_crosstab_phospho_SiteID_corrected.txt", sep = "\t")
+setwd(
+"~/OneDrive - PNNL/Documents/GitHub/Exp21_NRAS_ASO_treated_patients/proteomics/"
+)
+meta.df <- readxl::read_excel("Experiment 21 - Patient Samples_F.xlsx", 
+                              sheet = 2)
+global.df <- read.table(
+  "data/global_data/ptrc_ex21_crosstab_global_gene_corrected.txt", 
+  sep = "\t")
+phospho.df <- read.table(
+  "data/phospho_data/ptrc_ex21_crosstab_phospho_SiteID_corrected.txt", 
+  sep = "\t")
 
 # add column for feature names and then make it the first column
 global.df$Gene <- rownames(global.df)
 
 #### 2. Import BeatAML data ####
-beatAMLwd <- "~/OneDrive - PNNL/Documents/PTRC2/BeatAML Patient Data/"
-setwd(beatAMLwd)
-drug.BeatAML <- utils::read.csv(paste0(beatAMLwd, "AUC\ Data/drug_response.csv"))
-
 # import drug MOA annotations
 moa.BeatAML <- utils::read.csv(
   "~/OneDrive - PNNL/Documents/PTRC2/BeatAML_single_drug_moa.csv",
   stringsAsFactors = FALSE, fileEncoding = "latin1")
 
-# import global proteomics
-global.BeatAML <- utils::read.table(
-  paste0("~/OneDrive - PNNL/Documents/PTRC2/BeatAML Patient Data/",
-         "Proteomics and Quality Control/Ex10_global_data/",
-         "ptrc_ex10_crosstab_global_gene_corrected.txt"), 
-  sep = "\t", header = TRUE)
+# login to Synapse
+synLogin()
 
-# import phospho proteomics
-# phospho.BeatAML <- utils::read.table(
-#   paste0("~/OneDrive - PNNL/Documents/PTRC2/BeatAML Patient Data/",
-#          "Proteomics and Quality Control/Ex10_phospho_data/",
-#          "ptrc_ex10_crosstab_phospho_SiteID_corrected.txt"), 
-#   sep = "\t", header = TRUE)
+# load data from Synapse
+BeatAML_synapse_id <- c("BeatAML_drug_response_AUC.csv" = "syn51674470", 
+                        "BeatAML_proteomics_metadata.txt" = "syn25807733",
+                        "BeatAML_global_proteomics_corrected.txt" = "syn25714248",
+                        "BeatAML_phospho_proteomics_corrected.txt" = "syn25714921")
 
-# import metadata for global proteomics
-meta.BeatAML <- utils::read.table(
-  paste0("~/OneDrive - PNNL/Documents/PTRC2/BeatAML Patient Data/",
-         "Proteomics and Quality Control/Ex10_metadata.txt"), 
-  sep = "\t", header = TRUE)
+for (i in 1:length(synapse_id_map)) {
+  lapply(BeatAML_synapse_id, synapser::synGet, 
+         downloadLocation = names(BeatAML_synapse_id))
+}
 
-#### Step 4. Format data for DMEA ####
+drug.BeatAML <- read.csv(names(BeatAML_synapse_id)[1])
+meta.BeatAML <- read.table(names(BeatAML_synapse_id)[2])
+global.BeatAML <- read.table(names(BeatAML_synapse_id)[3])
+phospho.BeatAML <- read.table(names(BeatAML_synapse_id)[4])
+
+#### Step 4. Format BeatAML data for DMEA ####
 ## format drug sensitivity data frame
 # format drug.BeatAML wide (samples in first column, drug names for rest of columns)
 drug.BeatAML <- reshape2::dcast(drug.BeatAML, sample_id ~ inhibitor, value.var = "auc", fill = NA)
@@ -121,93 +122,101 @@ names(drug.BeatAML)[1] <- names(global.BeatAML)[1]
 #### 2. Run panSEA across contrasts for each exp, omics type ####
 setwd("~/OneDrive - PNNL/Documents/GitHub/Exp21_NRAS_ASO_treated_patients/proteomics/data/")
 omics <- c("global", "phospho")
-exps <- c(21, 12, 18)
-## prepare set annotations
-# generate gmt.features beforehand to save time
-if (omics == "global") {
-  msigdb.info <- msigdbr::msigdbr("Homo sapiens", "C2", "CP:KEGG")
-  
-  # extract necessary info into data frame
-  msigdb.info <- as.data.frame(msigdb.info[, c(
-    "gene_symbol",
-    "gs_name",
-    "gs_description"
-  )])
-  
-  gmt <- DMEA::as_gmt(
-    msigdb.info, "gene_symbol", "gs_name", min.per.set = 6,
-    descriptions = "gs_description"
-  ) 
-} else if (omics == "phospho") {
-  ksdb <- read.csv("ksdb_human_20231101.csv")
-  ksdb$SUB_SITE <- paste0(ksdb$SUBSTRATE, ksdb$SUB_MOD_RSD, collapse = "_")
-  gmt <- DMEA::as_gmt(ksdb, "SUB_SITE", "KINASE", min.per.set = 6, 
-                      descriptions = "KIN_ACC_ID")
-}
-
 for (k in 1:length(omics)) {
-  for (m in 1:length(exps)) {
-    ## prepare input parameters
-    # identify contrasts
-    treatments <- na.omit(unique(meta.df$Treatment))
-    contrasts <- c()
-    for (i in 1:length(treatments)) {
-      for (j in 1:length(treatments)) {
-        # only pair different treatments
-        if (i != j) {
-          # put treatments in alphabetical order
-          alpha.pair <- sort(c(treatments[i], treatments[j]))
-          
-          # keep track of all treatment contrasts
-          contrasts <- c(contrasts,
-                         paste(alpha.pair[1], "vs.", alpha.pair[2]))
-        }
+  ## prepare set annotations
+  # generate gmt.features beforehand to save time
+  if (omics[k] == "global") {
+    msigdb.info <- msigdbr::msigdbr("Homo sapiens", "C2", "CP:KEGG")
+    
+    # extract necessary info into data frame
+    msigdb.info <- as.data.frame(msigdb.info[, c(
+      "gene_symbol",
+      "gs_name",
+      "gs_description"
+    )])
+    
+    gmt <- DMEA::as_gmt(
+      msigdb.info, "gene_symbol", "gs_name", min.per.set = 6,
+      descriptions = "gs_description"
+    ) 
+  } else if (omics[k] == "phospho") {
+    ksdb <- read.csv("ksdb_human_20231101.csv")
+    ksdb$SUB_SITE <- paste0(ksdb$SUBSTRATE, ksdb$SUB_MOD_RSD, collapse = "_")
+    gmt <- DMEA::as_gmt(ksdb, "SUB_SITE", "KINASE", min.per.set = 6, 
+                        descriptions = "KIN_ACC_ID")
+  }
+  
+  ## prepare other input parameters
+  # identify contrasts
+  treatments <- na.omit(unique(meta.df$Treatment))
+  contrasts <- c()
+  for (i in 1:length(treatments)) {
+    for (j in 1:length(treatments)) {
+      # only pair different treatments
+      if (i != j) {
+        # put treatments in alphabetical order
+        alpha.pair <- sort(c(treatments[i], treatments[j]))
+        
+        # keep track of all treatment contrasts
+        contrasts <- c(contrasts,
+                       paste(alpha.pair[1], "vs.", alpha.pair[2]))
       }
     }
-    contrasts <- unique(contrasts)
+  }
+  contrasts <- unique(contrasts)
+  
+  # identify column names containing data for each treatment
+  # add X in front of tube names to match column names 
+  # after import (e.g., X5 for tube 5)
+  sample.groups <- list()
+  for (i in 1:length(treatments)) {
+    sample.groups[[treatments[i]]] <- 
+      paste0("X", na.omit(meta.df[meta.df$Treatment == treatments[i], ]$Tube))
+  }
+  
+  
+  # run panSEA for each omics type across all contrasts
+  # CAUTION: this only works because the # of samples for each treatment type is equal
+  # otherwise would have to run panSEA for each contrast separately and 
+  # perhaps set the group.samples input parameter for panSEA
+  
+  # assemble inputs
+  data.list <- list()
+  expr.BeatAML <- list()
+  gmt.features <- list()
+  types <- contrasts
+  for (i in 1:length(contrasts)) {
+    # identify treatments in this contrast
+    contrast.treatments <- stringr::str_split(contrasts[i], " vs. ")[[1]]
     
-    # identify column names containing data for each treatment
-    # add X in front of tube names to match column names 
-    # after import (e.g., X5 for tube 5)
-    sample.groups <- list()
-    for (i in 1:length(treatments)) {
-      sample.groups[[treatments[i]]] <- 
-        paste0("X", na.omit(meta.df[meta.df$Treatment == treatments[i], ]$Tube))
-    }
+    # assemble each input data set for each contrast
+    data.list[[contrasts[i]]] <- 
+      global.df[ , c("Gene", sample.groups[[contrast.treatments[1]]],
+                     sample.groups[[contrast.treatments[3]]])]
     
+    expr.BeatAML[[contrasts[i]]] <- global.BeatAML
     
-    # run panSEA for each omics type across all contrasts
-    # CAUTION: this only works because the # of samples for each treatment type is equal
-    # otherwise would have to run panSEA for each contrast separately and 
-    # perhaps set the group.samples input parameter for panSEA
-    
-    # assemble inputs
-    data.list <- list()
-    expr.BeatAML <- list()
-    gmt.features <- list()
-    types <- contrasts
-    for (i in 1:length(contrasts)) {
-      # identify treatments in this contrast
-      contrast.treatments <- stringr::str_split(contrasts[i], " vs. ")[[1]]
-      
-      # assemble each input data set for each contrast
-      data.list[[contrasts[i]]] <- 
-        global.df[ , c("Gene", sample.groups[[contrast.treatments[1]]],
-                       sample.groups[[contrast.treatments[3]]])]
-      
-      expr.BeatAML[[contrasts[i]]] <- global.BeatAML
-      
-      gmt.features[[i]] <- gmt
-    }
-    
-    #run panSEA on global proteomics with BeatAML database
-    global.panSEA.BeatAML <- panSEA::panSEA(data.list, types, 
-                                            gmt.features = gmt.features,
-                                            gmt.drugs = DMEA::as_gmt(moa.BeatAML),
-                                            drug.sensitivity = drug.BeatAML,
-                                            expression = expr.BeatAML)
-    
-    #### 3. Upload results to Synapse ####
-    saveRDS(global.panSEA.BeatAML, file="exp21_global_panSEA_BeatAML.rds")
+    gmt.features[[i]] <- gmt
+  }
+  
+  # run panSEA by querying BeatAML data
+  global.panSEA.BeatAML <- panSEA::panSEA(data.list, types, 
+                                          gmt.features = gmt.features,
+                                          gmt.drugs = DMEA::as_gmt(moa.BeatAML),
+                                          drug.sensitivity = drug.BeatAML,
+                                          expression = expr.BeatAML)
+  
+  ## upload results to Synapse
+  saveRDS(global.panSEA.BeatAML, file="exp21_global_panSEA_BeatAML.rds")
+  synapse_id_map <- c("syn51409382" = "data/global_data/", 
+                      "syn51409382" = "data/phospho_data/")
+  # instead of crosstabs, change to upload panSEA results
+  for (i in 1:length(synapse_id_map)) {
+    crosstabs <- list.files(path = synapse_id_map[i],
+                            pattern = "crosstab.*.txt",
+                            full.names = T)
+    files <- lapply(crosstabs, synapser::File,
+                    parent = names(synapse_id_map)[i])
+    lapply(files, synapser::synStore)
   }
 }
