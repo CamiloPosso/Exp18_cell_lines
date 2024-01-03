3e5716253b26676b15ae17ce4f46b9d79ca76e28
Belinda B. Garana <75867404+BelindaBGarana@users.noreply.github.com>
2024-01-02 18:44:20 -0800

    Update R files for Chr8 analysis

--- proteomics/6-panSEA_global,phospho.R
+++ proteomics/6-panSEA_global,phospho.R
@@ -1,139 +1,32 @@
-# Differential expression & enrichment analyses: global & phospho
-# PTRC2: exp 22
+# Differential expression & enrichment analyses: global
+# Chr8
 # Author: Belinda B. Garana
 # Created: 2023-12-06
-# Last edit: 2023-12-14
+# Last edit: 2024-01-02
 
 library(readxl); library(panSEA); library(synapser)
 library(stringr); library(tidyr)
 
 #### 1. Import metadata & crosstabs ####
 setwd(
-"~/OneDrive - PNNL/Documents/GitHub/Exp22_NRAS_ASO_treated_patients/proteomics/data/"
+"~/OneDrive - PNNL/Documents/GitHub/Chr8/proteomics/data/"
 )
-meta.df <- readxl::read_excel("Experiment 22 - Patient Samples_F.xlsx", 
-                              sheet = 2)
+meta.df <- readxl::read_excel("Chr8-MetaDataSheet.xlsx")
 global.df <- read.table(
-  "global_data/ptrc_ex22_crosstab_global_gene_corrected.txt", 
+  "global_data/Chr8_crosstab_global_gene_corrected.txt", 
   sep = "\t")
-phospho.df <- read.table(
-  "phospho_data/ptrc_ex22_crosstab_phospho_SiteID_corrected.txt", 
+global.w.mouse.df <- read.table(
+  "global_data/Chr8-with-mouse_crosstab_global_gene_corrected.txt", 
   sep = "\t")
 
 # add column for feature names and later make it the first column
 global.df$Gene <- rownames(global.df)
-phospho.df$SUB_SITE <- rownames(phospho.df)
-
-#### 2. Import BeatAML data ####
-# import drug MOA annotations
-moa.BeatAML <- utils::read.csv(
-  "~/OneDrive - PNNL/Documents/PTRC2/BeatAML_single_drug_moa.csv",
-  stringsAsFactors = FALSE, fileEncoding = "latin1")
-
-# login to Synapse
-synLogin()
-
-# load data from Synapse
-BeatAML.path <- "BeatAML_DMEA_inputs"
-BeatAML_synapse_id <- list("drug_response.csv" = "syn51674470", 
-                        "Ex10_metadata.txt" = "syn25807733",
-                        "ptrc_ex10_crosstab_global_gene_corrected.txt" = "syn25714248",
-                        "ptrc_ex10_crosstab_phospho_siteID_corrected.txt" = "syn25714921")
-lapply(BeatAML_synapse_id, synapser::synGet, downloadLocation = BeatAML.path)
-
-setwd(BeatAML.path)
-drug.BeatAML <- read.csv(names(BeatAML_synapse_id)[1])
-meta.BeatAML <- read.table(names(BeatAML_synapse_id)[2], sep = "\t", 
-                           header = TRUE)
-global.BeatAML <- read.table(names(BeatAML_synapse_id)[3], sep = "\t", 
-                             header = TRUE)
-phospho.BeatAML <- read.table(names(BeatAML_synapse_id)[4], sep = "\t", 
-                              header = TRUE)
-
-#### Step 4. Format BeatAML data for DMEA ####
-sample.names <- "Barcode.ID"
-
-## format drug sensitivity data frame
-# format drug.BeatAML wide (samples in first column, drug names for rest of columns)
-drug.BeatAML <- reshape2::dcast(drug.BeatAML, sample_id ~ inhibitor, 
-                                value.var = "auc", fill = NA)
-
-# remove drugs without moa annotations and drug combos
-valid.drugs <- 
-  names(drug.BeatAML)[names(drug.BeatAML) %in% 
-                        moa.BeatAML[!is.na(moa.BeatAML),]$Drug] # 167 drugs
-drug.BeatAML <- drug.BeatAML[ , c("sample_id", valid.drugs)] # 167 drugs
-moa.BeatAML <- 
-  moa.BeatAML[moa.BeatAML$Drug %in% names(drug.BeatAML)[2:ncol(drug.BeatAML)], ]
-
-# change sample column name to match expression data
-names(drug.BeatAML)[1] <- sample.names
-
-## format global proteomics data frame
-# change global.BeatAML column names from SampleID.abbrev to 
-# Barcode.ID to match drug.BeatAML
-global.ids <- names(global.BeatAML)
-
-# remove X and any 0's from start of each column name and then
-# replace SampleID.abbrev with Barcode.ID to match drug.BeatAML
-for(i in seq_len(length(global.ids))){
-  global.ids[i] <- substr(global.ids[i], 2, nchar(global.ids[i]))
-  
-  if(substring(global.ids[i], 1, 1) == 0){
-    global.ids[i] <- substr(global.ids[i], 2, nchar(global.ids[i]))
-  }
-  
-  if(global.ids[i] %in% meta.BeatAML$SampleID.abbrev){
-    global.ids[i] <- meta.BeatAML[meta.BeatAML$SampleID.abbrev == global.ids[i], ]$Barcode.ID
-  }
-}
-
-# replace global.BeatAML column names 
-names(global.BeatAML) <- global.ids
-
-# transpose global.BeatAML so that first column is Barcode.ID and 
-# rest of columns are gene symbols
-global.BeatAML <- as.data.frame(t(global.BeatAML))
-
-# make first column Barcode.ID
-global.BeatAML[, sample.names] <- rownames(global.BeatAML)
-global.BeatAML <- 
-  global.BeatAML[ , c(sample.names, 
-                      names(global.BeatAML[ , 1:(ncol(global.BeatAML)-1)]))]
-
-## format phospho-proteomics data frame
-# change global.BeatAML column names from SampleID.abbrev to Barcode.ID to match drug.BeatAML
-phospho.ids <- names(phospho.BeatAML)
-
-# remove X and any 0's from start of each column name and then
-# replace SampleID.abbrev with Barcode.ID to match drug.BeatAML
-for(i in seq_len(length(phospho.ids))){
-  phospho.ids[i] <- substr(phospho.ids[i], 2, nchar(phospho.ids[i]))
-
-  if(substring(phospho.ids[i], 1, 1) == 0){
-    phospho.ids[i] <- substr(phospho.ids[i], 2, nchar(phospho.ids[i]))
-  }
-
-  if(phospho.ids[i] %in% meta.BeatAML$SampleID.abbrev){
-    phospho.ids[i] <- meta.BeatAML[
-      meta.BeatAML$SampleID.abbrev == phospho.ids[i], ]$Barcode.ID
-  }
-}
-
-# replace phospho.BeatAML column names
-names(phospho.BeatAML) <- phospho.ids
-
-# transpose phospho.BeatAML so that first column is Barcode.ID and rest of columns are gene symbols
-phospho.BeatAML <- as.data.frame(t(phospho.BeatAML))
-
-# make first column Barcode.ID
-phospho.BeatAML[, sample.names] <- rownames(phospho.BeatAML)
-phospho.BeatAML <- phospho.BeatAML[ , c(sample.names, names(phospho.BeatAML[ , 1:(ncol(phospho.BeatAML)-1)]))]
+globalw.mouse.df$Gene <- rownames(global.w.mouse.df)
 
 #### 2. Run panSEA across contrasts for each exp, omics type ####
 # synapse IDs must match order of omics list
-# synapse_id_map <- c("syn53180717" = "global_data/",
-#                     "syn53180718" = "phospho_data/")
+# synapse_id_map <- c("syn" = "global_data/",
+#                     "syn" = "global_with_mouse_data/")
 
 ## prepare other input parameters
 # identify contrasts
@@ -141,21 +34,15 @@ treatments <- na.omit(unique(meta.df$Treatment))
 
 # set short-hand names for treatments for better plots
 # IMPORTANT: these must have the same order as treatments above
-treatment.names <- c("Ctl", "ASO", "ASO + Gilt", 
-                     "NRAS ASO", "NRAS ASO + Gilt")
+treatment.names <- c("Ref", "WU-487CPP1", "MN-2", "JH-2-055", "JH-2-079",
+                     "JH-2-002", "WU-22t Cshim81")
 
 # define types based on short-hand contrasts
-types <- c(
-  # compare everything to control
-  "ASO vs. Ctl", "ASO + Gilt vs. Ctl", 
-  "NRAS ASO vs. Ctl", "NRAS ASO + Gilt vs. Ctl",
-  
-  # compare to ASO
-  "ASO + Gilt vs. ASO", "NRAS ASO vs. ASO", 
-  
-  # compare to ASO + Gilt
-  "NRAS ASO + Gilt vs. ASO + Gilt"
-)
+# by comparing everything to ref
+types <- c()
+for (i in 1:(length(treatment.names) - 1)) {
+  types[i] <- paste(treatment.names[i + 1], "vs.", treatment.names[1])
+}
 
 # get contrasts with full-length names to extract data
 contrasts <- c()
@@ -182,15 +69,16 @@ for (i in 1:length(treatments)) {
 
 #all.files2 <- c(DEG.files, GSEA.files, DMEA.files)
 #subsets <- c("Differential expression", "GSEA", "DMEA")
-#setwd("~/OneDrive - PNNL/Documents/GitHub/Exp22_CRISPR-Cas9_NRAS_KO_cell_lines/proteomics/data/")
-base.path <- "~/OneDrive - PNNL/Documents/GitHub/Exp22_CRISPR-Cas9_NRAS_KO_cell_lines/proteomics/data/"
-omics <- c("global", "phospho")
+#setwd("~/OneDrive - PNNL/Documents/GitHub/Chr8/proteomics/data/")
+base.path <- "~/OneDrive - PNNL/Documents/GitHub/Chr8/proteomics/data/"
+#omics <- c("global", "phospho")
+omics <- c("global", "global-with-mouse")
 for (k in 1:length(omics)) {
   setwd(paste0(base.path, synapse_id_map[k]))
   
   ## prepare set annotations
   # generate gmt.features beforehand to save time
-  if (omics[k] == "global") {
+  if (grep("global", omics[k], ignore.case = TRUE)) {
     msigdb.info <- msigdbr::msigdbr("Homo sapiens", "C2", "CP:KEGG")
     
     # extract necessary info into data frame
@@ -205,48 +93,19 @@ for (k in 1:length(omics)) {
       descriptions = "gs_description"
     ) 
   } else if (omics[k] == "phospho") {
-    # # only create gmt the first time
-    # ksdb <- read.csv(paste0("https://raw.githubusercontent.com/BelindaBGarana/",
-    #                         "panSEA/shiny-app/data/ksdb_20231101.csv"))
-    # ksdb.human <- ksdb[
-    #   ksdb$KIN_ORGANISM == "human" & ksdb$SUB_ORGANISM == "human", ]
-    # ksdb <- NULL # save space
-    # # ksdb.human$SUB_SITE <- paste(ksdb.human$SUBSTRATE, ksdb.human$SUB_MOD_RSD,
-    # #                               collapse = "_")
-    # ksdb.human <- ksdb.human %>% unite("SUB_SITE",
-    #                                    c("SUBSTRATE", "SUB_MOD_RSD"),
-    #                                    sep = "-", remove = FALSE)
-    # 
-    # # remove last character from PNNL phospho feature IDs to match with KSDB
-    # # this would only work for substrates with only 1 site
-    # PNNL_SUB_SITE <- unique(c(phospho.df$SUB_SITE, phospho.BeatAML$SUB_SITE))
-    # SUB_SITE <- stringr::str_sub(PNNL_SUB_SITE, end = -2)
-    # phospho.ref <- data.frame(PNNL_SUB_SITE, SUB_SITE)
-    # phospho.ref$KSDB_SUB_SITE <- NA
-    # phospho.ref[phospho.ref$SUB_SITE %in% ksdb.human$SUB_SITE, 
-    #             ]$KSDB_SUB_SITE <- phospho.ref[
-    #               phospho.ref$SUB_SITE %in% ksdb.human$SUB_SITE, ]$SUB_SITE # 637 matches
-    # phospho.ref$KINASE <- lapply(as.list(phospho.ref$SUB_SITE), 
-    #                              stringr::str_split, sep="-")
-    # 
-    # gmt <- DMEA::as_gmt(ksdb.human, "SUB_SITE", "KINASE", min.per.set = 6,
-    #                     descriptions = "KIN_ACC_ID")
-    # ksdb.human <- NULL # save space
-    
-    # use PNNL phospho feature IDs instead of ksdb
-    # SUB_SITE <- phospho.df$SUB_SITE
-    # phospho.ref <- data.frame(SUB_SITE)
-    # phospho.ref <- phospho.ref %>% tidyr::extract(SUB_SITE, "KINASE", 
-    #                                               remove = FALSE)
-    # SUB_SITE <- NULL
-    # gmt <- DMEA::as_gmt(phospho.ref, "SUB_SITE", "KINASE", min.per.set = 6)
+    # only create gmt the first time
+    # and use PNNL phospho feature IDs instead of ksdb
+    SUB_SITE <- phospho.df$SUB_SITE
+    phospho.ref <- data.frame(SUB_SITE)
+    phospho.ref <- phospho.ref %>% tidyr::extract(SUB_SITE, "KINASE",
+                                                  remove = FALSE)
+    SUB_SITE <- NULL
+    gmt <- DMEA::as_gmt(phospho.ref, "SUB_SITE", "KINASE", min.per.set = 6)
 
     # save gmt for future analyses
-    #saveRDS(gmt, "gmt_ksdb_human_20231101.rds")
-    # saveRDS(gmt, "gmt_PNNL_kinase-substrate_PTRC2_exp22.rds")
+    saveRDS(gmt, "gmt_PNNL_kinase-substrate_Chr8_5344.rds")
     
-    #gmt <- readRDS("gmt_ksdb_human_20231101.rds")
-    gmt <- readRDS("gmt_PNNL_kinase-substrate_PTRC2_exp22.rds")
+    #gmt <- readRDS("gmt_PNNL_kinase-substrate_Chr8_5344.rds")
   }
   
   # run panSEA for each omics type across all contrasts
@@ -256,19 +115,22 @@ for (k in 1:length(omics)) {
   
   # assemble inputs
   data.list <- list()
-  expr.BeatAML <- list()
   gmt.features <- list()
   for (i in 1:length(contrasts)) {
     # identify treatments in this contrast
     contrast.treatments <- stringr::str_split(contrasts[i], " vs. ")[[1]]
     
     # assemble each input data set for each contrast
-    if (omics[k] == "global"){
-      data.list[[types[i]]] <- 
-        global.df[ , c("Gene", sample.groups[[contrast.treatments[1]]],
-                       sample.groups[[contrast.treatments[2]]])]
-      
-      expr.BeatAML[[types[i]]] <- global.BeatAML 
+    if (grep("global", omics[k], ignore.case = TRUE)){
+      if (grep("mouse", omics[k], ignore.case = TRUE)) {
+        data.list[[types[i]]] <- 
+          global.w.mouse.df[ , c("Gene", sample.groups[[contrast.treatments[1]]],
+                         sample.groups[[contrast.treatments[2]]])]
+      } else {
+        data.list[[types[i]]] <- 
+          global.df[ , c("Gene", sample.groups[[contrast.treatments[1]]],
+                         sample.groups[[contrast.treatments[2]]])]
+      }
       
       feature.names <- rep("Gene", length(types))
     } else if (omics[k] == "phospho") {
@@ -276,84 +138,72 @@ for (k in 1:length(omics)) {
         phospho.df[ , c("SUB_SITE", sample.groups[[contrast.treatments[1]]],
                        sample.groups[[contrast.treatments[2]]])]
       
-      expr.BeatAML[[types[i]]] <- phospho.BeatAML
-      
       feature.names <- rep("SUB_SITE", length(types))
     }
     
     gmt.features[[types[i]]] <- gmt
   }
   
-  # run panSEA by querying BeatAML data
+  # run panSEA by querying CCLE data
   #Sys.setenv('R_MAX_VSIZE'=32000000000)
   #library(usethis) 
   #usethis::edit_r_environ()
 
-  panSEA.BeatAML <- panSEA::panSEA(data.list, types, 
+  panSEA.results <- panSEA::panSEA(data.list, types, 
                                    feature.names = feature.names, 
-                                   gmt.features = gmt.features,
-                                   gmt.drugs = DMEA::as_gmt(moa.BeatAML, 
-                                                            min.per.set = 5),
-                                   drug.sensitivity = drug.BeatAML,
-                                   expression = expr.BeatAML, 
-                                   min.per.set = 5)
-  
-  # phospho didn't have enough drug sets for DMEA
-  # Error in GSEA_custom(input, gmt, num.permutations, stat.type, min.per.set,  : 
-  #                        annotations for 2+ drug sets are required
-  # had to change min.per.set to 5
+                                   gmt.features = gmt.features)
   
   ## save results & upload to Synapse
   # store all results locally
   dir.create("analysis")
   setwd("analysis")
-  #saveRDS(panSEA.BeatAML, file=paste0("exp22_", omics[k], "_panSEA_BeatAML.rds")) # 8.5 GB for 7 contrasts
-  panSEA.BeatAML <- readRDS(paste0("exp22_", omics[k], "_panSEA_BeatAML.rds"))
+  #saveRDS(panSEA.results, file=paste0("Chr8_", omics[k], "_panSEA_CCLE.rds")) # 8.5 GB for 7 contrasts
+  panSEA.results <- readRDS(paste0("Chr8_", omics[k], "_panSEA_CCLE.rds"))
   
   # set file names
   DEG.files <- list("Differential_expression_results.csv" = 
-                      panSEA.BeatAML$mDEG.results$compiled.results$results,
+                      panSEA.results$mDEG.results$compiled.results$results,
                     "Differential_expression_mean_results.csv" =
-                      panSEA.BeatAML$mDEG.results$compiled.results$mean.results,
+                      panSEA.results$mDEG.results$compiled.results$mean.results,
                     "Differential_expression_correlation_matrix.pdf" =
-                      panSEA.BeatAML$mDEG.results$compiled.results$corr.matrix,
+                      panSEA.results$mDEG.results$compiled.results$corr.matrix,
                     "Differential_expression_dot_plot.pdf" =
-                      panSEA.BeatAML$mDEG.results$compiled.results$dot.plot)
+                      panSEA.results$mDEG.results$compiled.results$dot.plot)
   DMEA.files <- list("DMEA_results.csv" =
-                       panSEA.BeatAML$mDMEA.results$compiled.results$results,
+                       panSEA.results$mDMEA.results$compiled.results$results,
                      "DMEA_mean_results.csv" =
-                       panSEA.BeatAML$mDMEA.results$compiled.results$mean.results,
+                       panSEA.results$mDMEA.results$compiled.results$mean.results,
                      "DMEA_correlation_matrix.pdf" =
-                       panSEA.BeatAML$mDMEA.results$compiled.results$corr.matrix,
+                       panSEA.results$mDMEA.results$compiled.results$corr.matrix,
                      "DMEA_dot_plot.pdf" =
-                       panSEA.BeatAML$mDMEA.results$compiled.results$dot.plot,
+                       panSEA.results$mDMEA.results$compiled.results$dot.plot,
                      "DMEA_interactive_network.graph.html" =
-                       panSEA.BeatAML$mDMEA.network$interactive)
+                       panSEA.results$mDMEA.network$interactive)
   if (omics[k] == "phospho") {
     KSEA.files <- list("KSEA_results.csv" =
-                         panSEA.BeatAML$mKSEA.results$compiled.results$results,
+                         panSEA.results$mKSEA.results$compiled.results$results,
                        "KSEA_mean_results.csv" =
-                         panSEA.BeatAML$mKSEA.results$compiled.results$mean.results,
+                         panSEA.results$mKSEA.results$compiled.results$mean.results,
                        "KSEA_correlation_matrix.pdf" =
-                         panSEA.BeatAML$mKSEA.results$compiled.results$corr.matrix,
+                         panSEA.results$mKSEA.results$compiled.results$corr.matrix,
                        "KSEA_dot_plot.pdf" =
-                         panSEA.BeatAML$mKSEA.results$compiled.results$dot.plot,
+                         panSEA.results$mKSEA.results$compiled.results$dot.plot,
                        "KSEA_interactive_network.graph.html" =
-                         panSEA.BeatAML$mKSEA.network$interactive)
+                         panSEA.results$mKSEA.network$interactive)
     all.files <- list('Differential expression' = DEG.files,
                       'KSEA' = KSEA.files,
                       'DMEA' = DMEA.files)
   } else {
     GSEA.files <- list("GSEA_results.csv" =
-                         panSEA.BeatAML$mGSEA.results$compiled.results$results,
+                         panSEA.results$mGSEA.results$compiled.results$results,
                        "GSEA_mean_results.csv" =
-                         panSEA.BeatAML$mGSEA.results$compiled.results$mean.results,
+                         panSEA.results$mGSEA.results$compiled.results$mean.results,
                        "GSEA_correlation_matrix.pdf" =
-                         panSEA.BeatAML$mGSEA.results$compiled.results$corr.matrix,
+                         panSEA.results$mGSEA.results$compiled.results$corr.matrix,
                        "GSEA_dot_plot.pdf" =
-                         panSEA.BeatAML$mGSEA.results$compiled.results$dot.plot,
+                         panSEA.results$mGSEA.results$compiled.results$dot.plot,
                        "GSEA_interactive_network.graph.html" =
-                         panSEA.BeatAML$mGSEA.network$interactive)
+                         panSEA.results$mGSEA.network$interactive)
     all.files <- list('Differential expression' = DEG.files,
                       'GSEA' = GSEA.files,
                       'DMEA' = DMEA.files)
@@ -412,5 +262,5 @@ for (k in 1:length(omics)) {
       lapply(HTMLs, synapser::synStore) 
     }
   }
-  panSEA.BeatAML <- NULL # make space to process next omics type
+  panSEA.results <- NULL # make space to process next omics type
 }
